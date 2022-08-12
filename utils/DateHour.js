dd0115d7375c538c01fe794b9bb6cd5504c134bf
João Inácio Neto <birobirobiro.dev@gmail.com>
2022-08-12 18:12:12 -0300

    chore: add easter egg

--- utils/DateHour.js
+++ utils/DateHour.js
@@ -2,8 +2,12 @@ var today = new Date();
 
 var date = today.getDate() + '/' + (today.getMonth() + 1).toString().padStart(2, '0') + '/' + today.getFullYear();
 
-var time = today.getHours().toString().padStart(2, '0') + ":" + today.getMinutes() + ":" + today.getSeconds().toString().padStart(2, '0');
+var time =
+  today.getHours().toString().padStart(2, '0') + ":" +
+  today.getMinutes().toString().padStart(2, '0'); + ":";
 
 var dateTime = date + ' - ' + time;
 
 document.getElementById('date-hour').innerHTML = dateTime;
+
+console.log(dateTime);
