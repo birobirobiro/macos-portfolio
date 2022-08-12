db33f1a82257e31fabb0ab826e51a0998c378e5c
João Inácio Neto <birobirobiro.dev@gmail.com>
2022-08-12 13:51:26 -0300

    fix: date/hour

--- utils/DateHour.js
+++ utils/DateHour.js
@@ -1,8 +1,8 @@
 var today = new Date();
 
-var date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
+var date = today.getDate() + '/' + (today.getMonth() + 1).toString().padStart(2, '0') + '/' + today.getFullYear();
 
-var time = today.getHours().toString().padStart(2, '0') + ":" + today.getMinutes();
+var time = today.getHours().toString().padStart(2, '0') + ":" + today.getMinutes() + ":" + today.getSeconds().toString().padStart(2, '0');
 
 var dateTime = date + ' - ' + time;
 
